49853a8fb2b74c471b8354c8f7d6cd97a5faa53e
TIMSAL <zehra.timsal@gmail.com>
2025-09-14 03:23:12 +0530

    event-arch-test-run

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+import json
+from fastapi import FastAPI, Request
 from services.metrics_generator import generate_metrics
 from langsmith import traceable
 
@@ -10,3 +11,20 @@ app = FastAPI()
 def main(userInput: HelloRequest):
     result=generate_metrics(userInput.name)
     return result
+
+
+@app.post("/test-event")
+async def handle_gcs_event(request: Request):
+    event = await request.json()
+    print("Received Eventarc event:", json.dumps(event, indent=2))
+
+    bucket = event["data"]["bucket"]
+    file_name = event["data"]["name"]
+
+    # 👉 Call your existing logic here
+    result = f"New file {file_name} uploaded in bucket {bucket}"
+    print(result)
+
+    return {"message": result}
+
+
