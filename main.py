430340fecd831c6956f261d77dd9ac1ddf792101
TIMSAL <zehra.timsal@gmail.com>
2025-09-15 21:09:37 +0530

    first

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import json
 from fastapi import FastAPI, Request
 from services.metrics_generator import generate_metrics
 from langsmith import traceable
-
+import uvicorn
 from models.summarize import HelloResponse,HelloRequest
 
 app = FastAPI()
@@ -18,13 +18,18 @@ async def handle_gcs_event(request: Request):
     event = await request.json()
     print("Received Eventarc event:", json.dumps(event, indent=2))
 
-    bucket = event["data"]["bucket"]
-    file_name = event["data"]["name"]
+    bucket = event["bucket"]
+    file_name = event["name"]
 
     # 👉 Call your existing logic here
     result = f"New file {file_name} uploaded in bucket {bucket}"
     print(result)
 
     return {"message": result}
+
+# if __name__ == "__main__":
+#     uvicorn.run("main:app")
+
+
 
 
